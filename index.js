77eac542eca8c253d9b0ec1a0f1cd52e8d32b838
coleworsley <cole.s.worsley@gmail.com>
2017-05-18 10:14:27 -0600

    update index.js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import bubbleSort from './scripts/bubbleSort.js';
 import insertionSort from './scripts/insertionSort.js';
-import mergeSort from './scripts/mergeSort.js';
+import { mergeSort } from './scripts/mergeSort.js';
 import quickSort from './scripts/quickSort.js';
 
 
